fb5ca21ae1f13b26ded4886e4070d60c84667f71
Max Tomago <noreply@tomagotech.com>
2023-11-01 15:02:29 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name = "mini",
+    version = "0.1.0",
+    packages = ["mini", "tests"]
+)
---
+++ tests/test_property.py
@@ -0,0 +1,20 @@
+import pytest
+from mini.property import Property
+
+
+def test_property():
+    prop = Property.from_str("name = Leon")
+    assert prop.key == "name"
+    assert prop.value == "Leon"
+
+
+def test_property2():
+    prop = Property.from_str("name=Leon")
+    assert prop.key == "name"
+    assert prop.value == "Leon"
+
+
+def test_property3():
+    prop = Property.from_str(" name = Leon ")
+    assert prop.key == "name"
+    assert prop.value == "Leon"
