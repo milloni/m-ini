153435ad85ae16e498cc30d5d97eef14045634c0
Max Tomago <noreply@tomagotech.com>
2023-11-01 17:54:36 +0100

    tests

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -2,7 +2,26 @@ import pytest
 from mini.tokenizer import Tokenizer, TokenKind
 
 
-def test_tokenizer():
+def test_tokenizer_good():
+    s = [
+        "[data]",
+        "name = Leon",
+        "",
+        "; semicolon indicates a comment",
+        "# and so does a hash",
+    ]
+    tokenizer = Tokenizer()
+    tokenizer.tokenize_stream(s)
+
+    # Trade off between test simplicity and performance. If not O(1) access, can be slow
+    assert tokenizer.tokens[0].kind == TokenKind.SECTION
+    assert tokenizer.tokens[1].kind == TokenKind.PARAMETER
+    assert tokenizer.tokens[2].kind == TokenKind.EMPTY
+    assert tokenizer.tokens[3].kind == TokenKind.COMMENT
+    assert tokenizer.tokens[4].kind == TokenKind.COMMENT
+
+
+def test_tokenizer_malformed():
     s = [
         "[data]",
         "name = Leon",
