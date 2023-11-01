448a101472232c549d810b3681ad710246665bcf
Max Tomago <noreply@tomagotech.com>
2023-11-01 17:48:31 +0100

    early tokenizer

---
+++ mini/tokenizer.py
@@ -0,0 +1,38 @@
+from enum import Enum
+
+
+class TokenKind(Enum):
+    SECTION = 1
+    PARAMETER = 2
+    COMMENT = 3
+    EMPTY = 4
+    MALFORMED = 5
+
+
+class Token:
+    def __init__(self, kind: TokenKind, raw_value: str):
+        self.kind = kind
+        self.raw_value = raw_value
+
+
+class Tokenizer:
+    def __init__(self):
+        self.tokens = []
+
+    def tokenize_stream(self, stream):
+        # For the purpose of this function, a stream is any object that supports iterating over it, where each element
+        # is thought to represent an entry in the configuration file.
+        for line in stream:
+            if line.startswith("[") and line.endswith("]"):
+                self.tokens.append(Token(TokenKind.SECTION, line))
+            elif line.startswith(";") or line.startswith("#"):
+                self.tokens.append(Token(TokenKind.COMMENT, line))
+            elif line == "":
+                self.tokens.append(Token(TokenKind.EMPTY, line))
+            elif "=" in line:
+                self.tokens.append(Token(TokenKind.PARAMETER, line))
+            else:
+                self.tokens.append(Token(TokenKind.MALFORMED, line))
+
+    def tokens(self):
+        return self.tokens
---
+++ tests/test_tokenizer.py
@@ -0,0 +1,23 @@
+import pytest
+from mini.tokenizer import Tokenizer, TokenKind
+
+
+def test_tokenizer():
+    s = [
+        "[data]",
+        "name = Leon",
+        "",
+        "; semicolon indicates a comment",
+        "# and so does a hash",
+        "[code",
+    ]
+    tokenizer = Tokenizer()
+    tokenizer.tokenize_stream(s)
+
+    # Trade off between test simplicity and performance. If not O(1) access, can be slow
+    assert tokenizer.tokens[0].kind == TokenKind.SECTION
+    assert tokenizer.tokens[1].kind == TokenKind.PARAMETER
+    assert tokenizer.tokens[2].kind == TokenKind.EMPTY
+    assert tokenizer.tokens[3].kind == TokenKind.COMMENT
+    assert tokenizer.tokens[4].kind == TokenKind.COMMENT
+    assert tokenizer.tokens[5].kind == TokenKind.MALFORMED
