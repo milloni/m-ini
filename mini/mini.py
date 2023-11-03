ed69d7c6f94713cf353fe4ace248b50fb88300af
Max Tomago <noreply@tomagotech.com>
2023-11-03 15:06:34 +0100

    Basic CLI

--- mini/mini.py
+++ mini/mini.py
@@ -2,16 +2,34 @@ import argparse
 from .tokenizer import Tokenizer
 
 
-def main():
+def create_parser():
     parser = argparse.ArgumentParser(description='INI parser')
+    parser.add_argument('command', type=str, help='available commands: validate, get')
     parser.add_argument('path', type=str, help='path to the INI file to be parsed')
+    return parser
+
+
+def main():
+    parser = create_parser()
     args = parser.parse_args()
-    path = args.path
-    with open(path, 'r', encoding="utf-8") as f:
+    with open(args.path, 'r', encoding="utf-8") as f:
         tokenizer = Tokenizer()
         tokenizer.tokenize_stream(f)
-    doc = tokenizer.construct_document()
-    print(doc.to_str())
+    try:
+        doc = tokenizer.construct_document()
+        is_valid = True
+    except ValueError as _:
+        is_valid = False
+    if args.command == 'validate':
+        if is_valid:
+            print('OK')
+        else:
+            print("Syntax error in INI file")
+    elif args.command == 'get':
+        pass
+    else:
+        print("Wrong command")
+        pass
 
 
 if __name__ == "__main__":
